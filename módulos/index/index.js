a314ca8647eeb93147c2d957c59519b1e0f45c59
Erick Costa <erick.costa.ti@gmail.com>
2019-12-04 17:54:20 -0300

    Trabalhando com módulos

--- módulos/index/index.js
+++ módulos/index/index.js
@@ -5,11 +5,24 @@ function addListItem(){
 
     var li = document.createElement('li');
 
+    var nome = document.getElementById('nome');
+
+    var email = document.getElementById('email');
+
+    console.log(nome.value);
+    console.log(email.value);
+
+    li.textContent = `Nome: ${nome.value} Sobrenome: ${email.value}`;
+
     ol.appendChild(li);
 
     document.body.appendChild(ol);
 
     console.log(indexList);
+
+    nome.value = "";
+
+    email.value = "";
 
     indexList++;
 }
